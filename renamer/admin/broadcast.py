5b2c1f7613c546209ec356783170c5bdea5250c4
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-21 00:11:53 +0530

    Update broadcast.py

--- renamer/admin/broadcast.py
+++ renamer/admin/broadcast.py
@@ -1,18 +1,42 @@
-# USED PREVIOUS 
 
-from pyrogram import filters
-
-from renamer.config import Config
-from pyrogram import Client as kinu6, filters
-
-
-@kinu6.on_message(
-    filters.private
-    & filters.command("broadcast")
-    & filters.user(Config.AUTH_USERS)
-    & filters.reply
-)
-async def broadcast_(c, m):
-    await c.start_broadcast(
-        broadcast_message=m.reply_to_message, admin_id=m.from_user.id
+from pyrogram import Client, filters
+import datetime
+import time
+from database.users_chats_db import db
+from ..config import Config
+from utils import broadcast_messages
+import asyncio
+        
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
+# https://t.me/GetTGLink/4178
+async def verupikkals(bot, message):
+    users = await db.get_all_users()
+    b_msg = message.reply_to_message
+    sts = await message.reply_text(
+        text='Broadcasting your messages...'
     )
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    blocked = 0
+    deleted = 0
+    failed =0
+
+    success = 0
+    async for user in users:
+        pti, sh = await broadcast_messages(int(user['id']), b_msg)
+        if pti:
+            success += 1
+        elif pti == False:
+            if sh == "Bocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
+                failed += 1
+        done += 1
+        await asyncio.sleep(2)
+        if not done % 20:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+    time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
