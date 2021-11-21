4e45a815ba9fe575b565a63732f30ee4220001d1
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-21 11:04:07 +0530

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -15,7 +15,7 @@ class Config:
     BOT_PASSWORD = os.environ.get("BOT_PASSWORD", "") if os.environ.get("BOT_PASSWORD", "") else None
     CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION") if os.environ.get("CUSTOM_CAPTION", "") else None
     FORCE_SUB = os.environ.get("FORCE_SUB", "") if os.environ.get("FORCE_SUB", "") else None
-    DATABASE_URI = os.environ.get("DATABASE_URL", "")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "")
     DATABASE_NAME = os.environ.get('DATABASE_NAME', "hashminner")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     try:
