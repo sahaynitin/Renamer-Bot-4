00e5cbb68134043c1b72242d4e34ee382ec8192d
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-11-30 18:23:19 +0530

    Update broadcast.py

--- renamer/plugins/broadcast.py
+++ renamer/plugins/broadcast.py
@@ -18,7 +18,7 @@ from ..config import Config
 
 broadcast_ids = {}
 
-BROADCAST_AS_COPY = config.BROADCAST_AS_COPY
+BROADCAST_AS_COPY = Config.BROADCAST_AS_COPY
 
 
 async def send_msg(user_id, message):
