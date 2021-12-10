5cddb2d7771771f6c82fb3da33c04a48f0006a50
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2021-12-11 01:49:55 +0530

    Update caption.py

--- renamer/plugins/caption.py
+++ renamer/plugins/caption.py
@@ -54,13 +54,14 @@ async def cb_handler(client: kinu6 , query: CallbackQuery):
             ]
         )
      )
-     elif data =="view_caption":
+       
+    elif data =="view_caption":
         try:
             caption = await get_caption(query.from_user.id)
             c_text = caption.caption
            
         except:
-            c_text = "Sorry but you haven't added any caption yet please set your caption through /caption command" 
+            c_text = "Sorry but you haven't added any caption yet please set your caption through /caption command"
            
         await query.message.edit(
             text=f"<b>Your Custom Caption:</b> \n\n{c_text} ",
@@ -75,28 +76,30 @@ async def cb_handler(client: kinu6 , query: CallbackQuery):
                 ]
             )
         )
-    
-     elif data == "del_caption":
+       
+    elif data == "del_caption":
         try:
-           await del_caption(query.from_user.id)   
+            await del_caption(query.from_user.id)
+           
         except:
             pass
         await query.message.edit_text(
             text="<b>caption deleted successfully</b>",
             disable_web_page_preview = True,
             reply_markup=InlineKeyboardMarkup(
-            [
                 [
-                    InlineKeyboardButton('Back', callback_data = "caption"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close_data")
+                    [
+                        InlineKeyboardButton('Back', callback_data = "caption"),
+                        InlineKeyboardButton("🔒 Close", callback_data = "close_data")
+                    ]
                 ]
-            ]
+            )
         )
-     )
-        
-     elif data == "close_data":
+       
+    elif data == "close_data":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
+           
         except:
             pass
